00fd6807cdbb82691c130e756bc996a675844ef6
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2023-10-07 22:53:50 +0530

    batch wise upload implemented

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-weekNo = "141"
+weekNo = "3"
 rootPath = "E:\Xiaomi Contract\THEMES\Week"
 path = rootPath+weekNo
 # tagList = ["Cute", "Light and shadow", "Simple", "UI", "Fairy", "Clock"]
--- script_main.py
+++ script_main.py
@@ -8,6 +8,8 @@ from selenium.webdriver.common.by import By
 import data
 import time
 
+maxTab = 5
+sequenceList = []
 
 mtzList = []
 
@@ -15,6 +17,16 @@ for (root, dirs, file) in os.walk(data.path):
     for f in file:
         if '.mtz' in f:
             mtzList.append(f)
+
+
+count = len(mtzList) // maxTab
+rem = len(mtzList) % maxTab
+if(rem != 0):
+    count += 1
+for i in range(count):
+    sequenceList.append(mtzList[i * maxTab:(i + 1) * maxTab])
+
+print(sequenceList)
 
 
 options = Options()
@@ -48,14 +60,17 @@ wait.until(EC.presence_of_element_located((By.XPATH, xpath_miui.dialogOkBtn)))
 webBrowser.find_element(By.XPATH, xpath_miui.dialogOkBtn).click()
 mainWindow = webBrowser.current_window_handle
 
-for i in range(len(mtzList)):
-    webBrowser.execute_script("window.open('about:blank',"+str(i)+");")
-    webBrowser.switch_to.window(str(i))
-    webBrowser.get(homeUrl)
-for i in range(len(mtzList)):
-    webBrowser.switch_to.window(str(i))
-    xpath_miui.uploadSingleMtz(webBrowser, wait, mtzList[i])
-    time.sleep(3)
-    webBrowser.close()
+for i in range(len(sequenceList)):
+    for j in range(len(sequenceList[i])):
+        webBrowser.execute_script("window.open('about:blank',"+str(j)+");")
+        webBrowser.switch_to.window(str(j))
+        webBrowser.get(homeUrl)
+    for j in range(len(sequenceList[i])):
+        webBrowser.switch_to.window(str(j))
+        xpath_miui.uploadSingleMtz(webBrowser, wait, sequenceList[i][j])
+        time.sleep(3)
+        webBrowser.close()
+    webBrowser.switch_to.window(mainWindow)
+
 webBrowser.switch_to.window(mainWindow)
 webBrowser.close()
